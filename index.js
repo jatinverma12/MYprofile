7ea029ad68805f126bc6d75192164188aac71c50
jatin-verma-1999 <jatin71_soe@jnu.ac.in>
2021-05-07 23:53:43 +0530

    Fdsfds

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var cards=document.getElementsByClassName('card');
 for(var i=0;i<cards.length;i++)
   cards[i].addEventListener('click',function(){
-    this.className='cardclicked';
+    if(this.classList.contains("cardclicked"))
+    {
+      this.classList.remove("cardclicked");
+      this.className='card';
+    }
+      
+    else
+    {
+      this.className='cardclicked';
+    }
+    
   });
 
 // Get the modal
